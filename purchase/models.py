2ef574f99e9a91b927f6b3f0b3e987499e363e84
mehrnoosh <mehrnooshdadashi@gmail.com>
2024-12-13 00:06:11 +0330

    Add user and purchaserequest models with their migrations

--- purchase/models.py
+++ purchase/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=50)
+    balance = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+class PurchaseRequest(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    coin_name = models.CharField(max_length=50)
+    quantity = models.DecimalField(max_digits=10, decimal_places=2)
+    dollar_value = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
